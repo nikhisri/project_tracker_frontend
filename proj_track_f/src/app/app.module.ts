7a6b146320932d6168b99e69ea6945a4c41a26ba
teresagit29@gmail.com <teresagit29@gmail.com>
2024-07-30 11:18:46 +0530

    project details

--- proj_track_f/src/app/app.module.ts
+++ proj_track_f/src/app/app.module.ts
@@ -1,18 +1,43 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { CommonModule } from '@angular/common';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TableComponent } from './components/table/table.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+import {MatCardModule} from '@angular/material/card';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TableComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatListModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatTableModule,
+    FormsModule,
+    MatInputModule,
+    MatCardModule,
+    MatSidenavModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+ }
---
+++ proj_track_f/src/app/components/table/table.component.ts
@@ -0,0 +1,49 @@
+import { Component, Input } from '@angular/core';
+export interface PeriodicElement {
+  trcode: string;
+  name: string;
+  start_date: string;
+  end_date: string;
+  due_in: number;
+  budget_value: number;
+  status: string;
+  owner: string;
+}
+export interface DashboardElement{
+  trcode: string;
+  name: string;
+  due_date: string;
+  status: string;
+}
+export interface TableData {
+  [key: string]: any;
+}
+@Component({
+  selector: 'app-table',
+  templateUrl: './table.component.html',
+  styleUrls: ['./table.component.css']
+})
+export class TableComponent {
+  @Input() dataSource: TableData[] = [];
+  displayedColumns: string[] = [];
+
+  ngOnInit(): void {
+    this.determineDisplayedColumns();
+  }
+ allKeys:any;
+  // Determine displayed columns dynamically based on the structure of the data
+  determineDisplayedColumns(): void {
+    if (this.dataSource.length > 0) {
+      // Get all unique keys from the data
+      this.allKeys = this.dataSource.reduce((keys, item) => {
+        return keys['concat'](Object.keys(item));
+      }, []);
+      this.displayedColumns = Array.from(new Set(this.allKeys));
+    }
+  }
+
+  // Check if a value is a string
+  isString(value: any): boolean {
+    return typeof value === 'string';
+  }
+}
